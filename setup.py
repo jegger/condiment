058adaa8b91fe5ae72aac6ba22d38b3e29070edb
Dominique Brunand <dominique@jegger.ch>
2023-01-09 09:20:02 +0100

    Poetry compatibility: Do not rely on direct import for version (setup.py) / use find_packages

--- setup.py
+++ setup.py
@@ -1,25 +1,43 @@
 #!/usr/bin/env python
 
+import codecs
+from setuptools import find_packages
 from distutils.core import setup
-from condiment import __version__
-from os.path import join, dirname
+from os.path import join, dirname, abspath
 
-README = open(join(dirname(__file__), 'README.md')).read()
+
+def read(rel_path):
+    here = abspath(dirname(__file__))
+    with codecs.open(join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name='condiment',
     py_modules=['condiment'],
     scripts=['scripts/condiment'],
-    version=__version__,
+    version="0.6",
     description='Conditionally include code according to environment variables',
-    long_description=README,
+    long_description=read("README.md"),
     author='Mathieu Virbel',
     author_email='mat@kivy.org',
     keywords=['python', 'preprocessor', 'meta', 'condiment', 'conditional'],
     platforms='all',
+    packages=find_packages(),
+    python_requires=">=3.6.5",
     url='http://github.com/tito/condiment',
     license='zlib',
     zip_safe=False,
+    install_requires = [],
     classifiers = [
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
